8b714479a84906457c51ab539c0e3955c0eaba23
wizardsambolton <sam99bolton@gmail.com>
2015-08-19 10:05:48 +0100

    fixed some indetnation stuff

--- main.js
+++ main.js
@@ -79,115 +79,116 @@ var actions = {
 
     },
 
-removeReminders: function () {
-    // set reminders to be an empty array, quickest way to clear it
-    localStorage.setItem('reminders', JSON.stringify([]));
-    speak("All reminders have been cleared")
-    document.getElementById('#resultplace').innerHTML = "All reminders have been cleared";
-},
-addReminder: function(term) {
-    var item = localStorage.getItem('reminders');
-    if (item == null) {
-      // if we've never stored any reminder, so use an empty array string
-      item =  "[]";
-    }
+    removeReminders: function () {
+        // set reminders to be an empty array, quickest way to clear it
+        localStorage.setItem('reminders', JSON.stringify([]));
+        speak("All reminders have been cleared")
+        document.getElementById('#resultplace').innerHTML = "All reminders have been cleared";
+    },
+    addReminder: function(term) {
+        var item = localStorage.getItem('reminders');
+        if (item == null) {
+            // if we've never stored any reminder, so use an empty array string
+            item =  "[]";
+        }
 
-    // take our string item and parse it into a real array
-    var reminders = JSON.parse(item);
-    // push the new term onto the list
-    reminders.push(term);
-    // stringify the reminders and save them back in storage
-    localStorage.setItem('reminders', JSON.stringify(reminders));
-    
-    speak('Saved to my memory')
-    document.getElementById('#resultplace').innerHTML = "Saved to my memory";
-},
-listReminders: function() {
-    var item = localStorage.getItem('reminders');
-    if (item == null) {
-      // nothing has ever been stored before
-      // return so we don't do the rest of this function
-      return speak("No reminders");
-    }
+        // take our string item and parse it into a real array
+        var reminders = JSON.parse(item);
+        // push the new term onto the list
+        reminders.push(term);
+        // stringify the reminders and save them back in storage
+        localStorage.setItem('reminders', JSON.stringify(reminders));
 
-    var reminders = JSON.parse(item);
+        speak('Saved to my memory')
+        document.getElementById('#resultplace').innerHTML = "Saved to my memory";
+    },
+    listReminders: function() {
+        var item = localStorage.getItem('reminders');
+        if (item == null) {
+            // nothing has ever been stored before
+            // return so we don't do the rest of this function
+            return speak("No reminders");
 
-    if(reminders.length == 0) {
-      // theres an empty array, but still no reminders
-      // return so we dont do the rest of this function
-      return speak("no reminders");
-    }
+        }
 
-    speak('your reminders are as follows');
-    for(var i=0; i < reminders.length; i++) {
-        speak(reminders[i]);
+        var reminders = JSON.parse(item);
 
-        // array.join() sticks all the elements together seperated by a separator
-        document.getElementById('#resultplace').innerHTML = reminders.join(", ");
-    }
-},
-d6: function () {
-    var d6 = Math.floor(Math.random() * 6);
-    speak(d6);
-    document.getElementById("#resultplace").innerHTML = d6;
-},
+        if(reminders.length == 0) {
+            // theres an empty array, but still no reminders
+            // return so we dont do the rest of this function
+            return speak("no reminders");
+        }
 
-diceroll1: function (term) {
+        speak('your reminders are as follows');
+        for(var i=0; i < reminders.length; i++) {
+            speak(reminders[i]);
 
-    if (mood == 'iffy' || mood == "not very good") {
-        speak("no")
-        document.getElementById('#resultplace').innerHTML = "no";
-    }
-    else {
-        var diceroll2 = Math.floor(Math.random() * term);
-        speak(diceroll2);
-        document.getElementById("#resultplace").innerHTML = diceroll2;
-    }
-},
+            // array.join() sticks all the elements together seperated by a separator
+            document.getElementById('#resultplace').innerHTML = reminders.join(", ");
+        }
+    },
+    d6: function () {
+        var d6 = Math.floor(Math.random() * 6);
+        speak(d6);
+        document.getElementById("#resultplace").innerHTML = d6;
+    },
 
-wolfram: function(term) {
-    $.get("https://api.wolframalpha.com/v2/query?input="+term+"&appid=THYQLJ-3K45Y2A7W5"), function(wolfram__) {
-        speak(wolfram__);
-    };
-    //http://api.wolframalpha.com/v2/query?input=pi&appid=XXXX
-    //var wolfram_ = "http://api.wolframalpha.com/v2/query?input="+term+"&appid=THYQLJ-3K45Y2A7W5";
-    //speak(wolfram_);
-},
-weather: function (term) {
-    $.get('https://api.openweathermap.org/data/2.5/weather?q='+term, function (theweather) {
-        speak(theweather.temp);
+    diceroll1: function (term) {
 
+        if (mood == 'iffy' || mood == "not very good") {
+            speak("no")
+            document.getElementById('#resultplace').innerHTML = "no";
+        }
+        else {
+            var diceroll2 = Math.floor(Math.random() * term);
+            speak(diceroll2);
+            document.getElementById("#resultplace").innerHTML = diceroll2;
+        }
+    },
 
-
-
-
-    });
-
-},
-badAsk: function() {
-    if (mood == 'not very good' || mood == 'iffy') {
-        _askBad = ['its not about me', 'nothing'];
-        askBad = _askBad[Math.floor(Math.random() * 2)];
-        speak(askBad);
-        document.getElementById("#resultplace").innerHTML = askBad;
-    }
-    else {}
-},
-
-mood: function() {
-    _mood = ['very good, thank you', 'not very good', 'fair', 'iffy', 'Awesome!', 'Good, thank you', 'Ready to Rock',];
-    mood = _mood[Math.floor(Math.random() * 7)];
-
-},
-
-howu: function () {
-
-    speak(mood);
-    document.getElementById("#resultplace").innerHTML = mood;
+    wolfram: function(term) {
+        $.get("https://api.wolframalpha.com/v2/query?input="+term+"&appid=THYQLJ-3K45Y2A7W5"), function(wolfram__) {
+            speak(wolfram__);
+        };
+        //http://api.wolframalpha.com/v2/query?input=pi&appid=XXXX
+        //var wolfram_ = "http://api.wolframalpha.com/v2/query?input="+term+"&appid=THYQLJ-3K45Y2A7W5";
+        //speak(wolfram_);
+    },
+    weather: function (term) {
+        $.get('https://api.openweathermap.org/data/2.5/weather?q='+term, function (theweather) {
+            speak(theweather.temp);
 
 
 
-},
+
+
+        });
+
+    },
+    badAsk: function() {
+        if (mood == 'not very good' || mood == 'iffy') {
+            _askBad = ['its not about me', 'nothing'];
+            askBad = _askBad[Math.floor(Math.random() * 2)];
+            speak(askBad);
+            document.getElementById("#resultplace").innerHTML = askBad;
+        }
+        else {}
+    },
+
+    mood: function() {
+        _mood = ['very good, thank you', 'not very good', 'fair', 'iffy', 'Awesome!', 'Good, thank you', 'Ready to Rock',];
+        mood = _mood[Math.floor(Math.random() * 7)];
+
+    },
+
+    howu: function () {
+
+        speak(mood);
+        document.getElementById("#resultplace").innerHTML = mood;
+
+
+
+    },
 
 
 };
