aa1189c611e813413fcd3127ed34007c40bebffd
wizardsambolton <sam99bolton@gmail.com>
2016-02-11 21:23:10 +0000

    add loading plugins, calling plugins

--- aubreyDB.js
+++ aubreyDB.js
@@ -12,13 +12,51 @@ var aubreyDB = {
         return JSON.parse(localStorage.getItem(this.DBname));
     },
 
+    _setItems:function(items){
+        if(typeof items != "object") {
+            throw "attempted attempt prevented";
+        }
+        localStorage.setItem(this.DBname,JSON.stringify(items));
+    },
+
+    _evalPlugin:function(plugin){
+        console.log("Loaded script " + plugin.body);
+    },
+
+
     boot:function(){
         if(!this._getItems()){
             this._init();
             console.log("Init complete");
         }
+        var items = this._getItems();
+        console.log("Loading saved plugins");
+        for (var i = 0; i < items.length; i++){
+            this._evalPlugin(items[i]);
+        }
         console.log("Boot complete!")
     },
+
+    loadPlugin:function(plugin){
+        var items = this._getItems();
+        for (var i = 0; i < items.length;i++) {
+            if(plugin.name == items[i].name){
+                console.log("Found duplicate plugin.");
+                return;
+            };
+        }
+        this._evalPlugin(plugin);
+        console.log("Storing plugin");
+        items.push(plugin);
+        this._setItems(items);
+
+    }
+
 };
 
 aubreyDB.boot();
+
+aubreyDB.loadPlugin({
+    name:"test random",
+    body:"var x = 'SPAGEET'"
+});
